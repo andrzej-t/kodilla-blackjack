ad5c297e96bb53ee6b1b28d06ded13f2b07dd9e5
andrzej-t <andrzej.tyrpa@gmail.com>
2021-03-15 11:50:48 +0100

    Changing background

--- src/main/java/com/kodilla/blackjack/Blackjack.java
+++ src/main/java/com/kodilla/blackjack/Blackjack.java
@@ -24,8 +24,8 @@ public class Blackjack extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
-        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        BackgroundSize backgroundSize = new BackgroundSize(200, 200, true, true, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
 
         GridPane grid = new GridPane();
